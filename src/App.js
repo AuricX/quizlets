79725289b87f21f195d0a7e85d6443c67a020b09
AuricX <auricx.business@gmail.com>
2025-10-18 11:40:58 +0300

    Installed TailwindCSS

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <><h1>Quizlet</h1><p>A simple flashcard app built with React.</p></>
+    <>
+      <h1 className="text-4xl font-bold">Quizlet</h1>
+      <p className="mt-2 text-lg">A simple flashcard app built with React.</p>
+    </>
   );
 }
 
