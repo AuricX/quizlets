ac7538f6a690f151c62b00d41a0819ae6939980f
mhd <ma9225533@gmail.com>
2025-11-19 18:18:28 +0200

    added course page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/Cou" element={<Cou />} />
         <Route element={<DashboardLayout />}>
           <Route path="/" element={<Dashboard />} />
           <Route path="/courses" element={<CoursesPage />} />
